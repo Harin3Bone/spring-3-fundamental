9443605698d13d5f876f934938aea958f3052358
Harin3Bone <harin.thananam@gmail.com>
2023-12-26 20:43:19 +0700

    Disable Spring auto password

--- src/main/java/com/tutorial/springfundamental/SpringFundamentalApplication.java
+++ src/main/java/com/tutorial/springfundamental/SpringFundamentalApplication.java
@@ -2,12 +2,13 @@ package com.tutorial.springfundamental;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class SpringFundamentalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringFundamentalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFundamentalApplication.class, args);
+    }
 
 }
